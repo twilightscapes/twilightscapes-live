60b28c76d5ce482cae4b8465fcbe2690380077ef
twilightscapes <ihatespam@gmail.com>
2021-08-05 10:06:35 -0500

    polish

--- src/components/animated-photos-menu.js
+++ src/components/animated-photos-menu.js
@@ -21,8 +21,10 @@ import { IoMdFingerPrint } from 'react-icons/io'
 
 const CustomBox = styled.div`
 
+
+
 @media (max-width: 48rem) {
-.container2 {height:300px !important;}
+.container2 {height:250px !important;}
 }
 
 `
@@ -44,7 +46,7 @@ const CaseStudyVertIndex = () => (
 
 
   
-<div className="container2" style={{display:'', justifySelf:'center', maxWidth:'1042px', height:'550px',  border:'5px solid ', margin:'5vh 12% 0 12%', borderRadius:'12px', boxShadow:'0 2px 3px 1px #777', transition:'background 0.3s ease-in'}}>
+<div className="container2" style={{display:'', justifySelf:'center', maxWidth:'1042px', height:'600px',  border:'5px solid ', margin:'5vh 12% 2rem 12%', borderRadius:'12px', boxShadow:'0 2px 3px 1px #FA02B7', transition:'background 0.3s ease-in'}}>
 
 
 
@@ -54,34 +56,58 @@ const CaseStudyVertIndex = () => (
 
     
 <section className="child" style={{position:'relative', height:'100%', overflow:''}}>
+
+<div style={{position:'absolute', bottom:'0',
+position:'absolute',
+bottom:'0',
+display:'block',
+width:'100%',
+textAlign:'center',
+background:'#222',
+fontSize:'20px',
+color:'#fff',
+opacity:'.8',
+}}>SCROLL DOWN FOR MORE</div>
+
+<Link state={{modal: true}} className="" to="/experiences/alien-egg-farm-naked">
 <div style={{display:'flex', alignItems:'center', justifyContent:'center', width:'100%', height:'100%', position:'absolute', }}>
       <div style={{alignSelf: 'flex-center', position:'absolute',  top:'2vh', textAlign:'center',}}>
-        <h2 style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}><Link state={{modal: true}} className="moreButton" to="/experiences/alien-egg-farm-naked">Enter the Experience</Link></h2>
+        <h2 className="moreButton" style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}>Enter the Experience</h2>
     </div>
   </div>
   <div style={{position:'absolute', zIndex:'-1'}}><StaticImage src="../img/interactive-photo-banner-alien-egg-farm.jpg" alt="Twilightscapes" className="featured-imaged" /></div>
+  </Link>
   </section>
 {/* END Panel 1 */}
 
 
 <section className="child" style={{position:'relative', height:'100%', overflow:''}}>
+
+  <Link state={{modal: true}} className="" to="/experiences/transcendence-naked">
+
 <div style={{display:'flex', alignItems:'center', justifyContent:'center', width:'100%', height:'100%', position:'absolute', }}>
 <div style={{alignSelf: 'flex-center', position:'absolute',  top:'2vh', textAlign:'center',}}>
-        <h2 style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}><Link state={{modal: true}} className="moreButton" to="/experiences/transcendence-naked">Enter the Experience</Link></h2>
+        <h2 className="moreButton" style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}>Enter the Experience</h2>
     </div>
   </div>
   <div style={{position:'absolute', zIndex:'-1'}}><StaticImage src="../img/interactive-photo-banner-transcendence.jpg" alt="Twilightscapes" className="featured-imaged" /></div>
+  
+  </Link>
   </section>
 {/* END Panel 2 */}
 
 
 <section className="child" style={{position:'relative', height:'100%', overflow:''}}>
+
+  <Link state={{modal: true}} to="/experiences/crude-addiction-naked">
+
 <div style={{display:'flex', alignItems:'center', justifyContent:'center', width:'100%', height:'100%', position:'absolute', }}>
 <div style={{alignSelf: 'flex-center', position:'absolute',  top:'2vh', textAlign:'center',}}>
-        <h2 style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}><Link state={{modal: true}} className="moreButton" to="/experiences/crude-addiction-naked">Enter the Experience</Link></h2>
+        <h2 className="moreButton" style={{fontSize:'100%', animation: 'fade 3s forwards', opacity:'0'}}>Enter the Experience</h2>
     </div>
   </div>
   <div style={{position:'absolute', zIndex:'-1'}}><StaticImage src="../img/interactive-photo-banner-crude-addiction.jpg" alt="Twilightscapes" className="featured-imaged" /></div>
+  </Link>
   </section>
 {/* END Panel 2 */}
 
