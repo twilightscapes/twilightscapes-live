3b7539f143f1c96f999905eb993639510f549566
twilightscapes <ihatespam@gmail.com>
2021-08-12 05:45:30 -0500

    footer installer tweaks

--- src/components/Install-footer.js
+++ src/components/Install-footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 // import { navigate } from 'gatsby-link'
-import { FiShare } from 'react-icons/fi';
+// import { FiShare } from 'react-icons/fi';
 import { FaRegPlusSquare } from 'react-icons/fa';
 import { IoShareOutline } from 'react-icons/io5';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-import ScrollAnimation from 'react-animate-on-scroll'
-import CookieConsent from "react-cookie-consent"
-import styled from "styled-components";
+// import ScrollAnimation from 'react-animate-on-scroll'
+// import CookieConsent from "react-cookie-consent"
+// import styled from "styled-components";
 import { StaticImage } from "gatsby-plugin-image"
 
 export default class Installer extends React.Component {
@@ -30,35 +30,36 @@ export default class Installer extends React.Component {
 
    render(){
        return(
-           <div className="no-app">
-           <div style={{textAlign:'center', }}>
-           <button onClick={this.handleShow} style={{borderBottom:'1px solid'}}>Install the Web App</button> - Get 30% OFF!
+           <div>
+           <div style={{textAlign:'center', marginTop:'1rem', }}>
+           <button onClick={this.handleShow} style={{borderBottom:'1px solid'}}>Install the Web App</button> &nbsp; | &nbsp; Get 30% OFF! 
              
              
              </div>
 
 {this.state.isActive ? 
 <>
-  <div style={{fontSize:'90%', textAlign:'center', display:'flex', flexDirection:'column', verticalAlign:'middle', lineHeight:'200%', width:'90%', margin:'0 auto', padding:' .5rem 1rem', border:'0px solid #333', borderRadius:'12px', maxWidth:'70vw' }}>
+  <div className="installwindow" style={{fontSize:'90%',  display:'flex', flexDirection:'column', verticalAlign:'middle', lineHeight:'200%', width:'90%', margin:'0 auto', padding:' .5rem', border:'0px solid #333', borderRadius:'12px', maxWidth:'100vw', textAlign:'center' }}>
   
-One-Click Install - NO APP STORE NEEDED
+  One-Click Install - NO APP STORE NEEDED
 <div style={{fontSize:'90%', textAlign:'center', display:'flex', flexDirection:'column', verticalAlign:'middle', lineHeight:'200%', width:'90%', margin:'0 auto', padding:' .5rem 1rem', border:'1px solid #333', borderRadius:'12px', backdropFilter:'blur(14px)', background:'rgba(0,0,0,0.60)', position:'relative', color:'#fff'}}>
       
-<button onClick={this.handleHide} style={{position:'absolute', right:'10px', top:'10px', fontSize:'24px'}}><AiOutlineCloseCircle /></button>
+<button onClick={this.handleHide} style={{position:'absolute', right:'-5px', top:'-5px', fontSize:'24px'}}><AiOutlineCloseCircle /></button>
 
-Simply click the "install" icon on the right-side of your browser address bar
+<p style={{maxWidth:'', textAlign:'center'}}>Simply click the "install" icon in your browser address bar
+</p>
 
-
-
-  <span style={{fontSize:'80%'}}>For Apple iOS users:</span>
+<div style={{border:'0px solid red', display:'block', width:'100px', height:'100px', position:'absolute', bottom:'0', right:'0', zIndex:'-1', overflow:'hidden', }}><StaticImage src="../../static/assets/installed-bg.png" alt=""  />
+</div>
+  <span style={{fontSize:'100%'}}>OR</span>
   <div style={{fontSize:'90%', textAlign:'center', display:'flex', flexDirection:'column', verticalAlign:'middle', lineHeight:'200%', width:'90%', margin:'0 auto', padding:' .5rem 1rem', border:'0px solid #333', borderRadius:'12px', }}>
     
-    <div style={{textAlign:'left', display:'flex', alignContent:'center', marginBottom:'1rem',}}>
+    <div style={{textAlign:'center', display:'flex', alignContent:'center', marginBottom:'1rem',}}>
    <IoShareOutline style={{fontSize:'200%', marginLeft:'0', marginRight:'2%', float:'',}} />
     1) Press the 'Share' button 
   </div>
   
-   <div style={{textAlign:'left', display:'flex', alignContent:'center',}}>
+   <div style={{textAlign:'center', display:'flex', alignContent:'center',}}>
    <FaRegPlusSquare style={{fontSize:'200%', marginLeft:'0', marginRight:'2%', float:'',}} /> 
    
   2) Press 'Add to Home Screen'
