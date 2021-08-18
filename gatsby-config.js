6bfe359b595cb221dd6f01b410b88ed130aab4e9
twilightscapes <ihatespam@gmail.com>
2021-08-18 08:55:39 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
         shopifyConnections: ["collections"],
+        salesChannel:"Secure3",
         shopName: ["checkout.securestore.shop"],
       },
     },
