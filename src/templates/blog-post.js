237eba96f3f437e893edb8b16ad923e0c963ffa1
twilightscapes <ihatespam@gmail.com>
2021-09-24 02:05:30 -0500

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -272,7 +272,7 @@ const iframeUrl = "https://www.youtube.com/embed/" + frontmatter.youtuber + ""
          
               config={{
                 youtube: {
-                  playerVars: { showinfo: 0, start:YouTubeStart, end:YouTubeEnd, autoplay: 1,}
+                  playerVars: { showinfo:0, start:YouTubeStart, end:YouTubeEnd, autoplay:1, mute:1}
                 },
               }}
               loop
