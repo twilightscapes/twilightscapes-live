b98c45484f8a9dbc64d95e7fbfde83df60af45aa
twilightscapes <ihatespam@gmail.com>
2022-01-16 23:59:41 -0600

    new dynamic home page gallery

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -14,10 +14,13 @@ import ReactPlayer from 'react-player/lazy'
 // import { ImPlay } from "react-icons/im"
 import styled from "styled-components"
 import Newsignup from "../components/newssign"
-import Portfolio from "../pages/portfolio"
+// import Portfolio from "../pages/portfolio"
 import BlogListHome from "../components/blog-list-home"
 import { Seo } from "../components/seo"
 import { Layout } from "../components/layout"
+import GalleryMenu from "../components/galleryMenu"
+import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
+import TwilightLogo from "../../static/assets/TSidebarHover.svg"
 const CustomBox = styled.div`
 
 `
@@ -26,6 +29,24 @@ const CustomBox = styled.div`
 
 export const pageQuery = graphql`
   query HomeQuery($id: String! ) {
+    
+
+    allFile(
+      filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, dir: {regex: "/img/faves/"}}
+      sort: { order: ASC, fields: name }
+    ) {
+      edges {
+        node {
+          name
+          id
+          relativePath
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+
     
     site {
       siteMetadata {
@@ -38,6 +59,8 @@ export const pageQuery = graphql`
         companyname
         showfooter
       }
+
+      
 
     }
     markdownRemark(id: { eq: $id }) {
@@ -88,6 +111,10 @@ export const pageQuery = graphql`
 
 
  
+  
+
+    
+
     
 
     posts: allMarkdownRemark(
@@ -230,7 +257,65 @@ const YouTube = frontmatter.youtuber
   }
 
   
-
+  const options = {
+    settings: {
+      autoplaySpeed: 4000,
+      boxShadow: '0px 0px 20px #000',
+      disableKeyboardControls: false,
+      disablePanzoom: false,
+      disableWheelControls: true,
+      hideControlsAfter: false,
+      lightboxTransitionSpeed: 0.3,
+      lightboxTransitionTimingFunction: 'linear',
+      overlayColor: 'rgba(0, 0, 0, 0.8)',
+      slideAnimationType: 'slide',
+      slideSpringValues: [300, 50],
+      slideTransitionSpeed: 0.6,
+      slideTransitionTimingFunction: 'linear',
+      usingPreact: false
+    },
+    buttons: {
+      backgroundColor: '#FA02B7',
+      iconColor: 'rgba(255, 255, 255, 0.8)',
+      iconPadding: '10px',
+      showAutoplayButton: false,
+      showCloseButton: true,
+      showDownloadButton: false,
+      showFullscreenButton: false,
+      showNextButton: false,
+      showPrevButton: false,
+      showThumbnailsButton: false,
+      size: '40px'
+    },
+    caption: {
+  captionAlignment: 'start',
+  captionColor: '#FFFFFF',
+  captionContainerPadding: '20px 12% 30px 12%',
+  captionFontFamily: 'inherit',
+  captionFontSize: 'inherit',
+  captionFontStyle: 'inherit',
+  captionFontWeight: 'inherit',
+  captionTextTransform: 'inherit',
+  showCaption: false
+    },
+    thumbnails: {
+      showThumbnails: false,
+      thumbnailsAlignment: 'center',
+      thumbnailsContainerBackgroundColor: '#111',
+      thumbnailsContainerPadding: '0',
+      thumbnailsGap: '0 2px',
+      thumbnailsIconColor: '#ffffff',
+      thumbnailsOpacity: 0.4,
+      thumbnailsPosition: 'bottom',
+      thumbnailsSize: ['100px', '80px']
+    },
+    progressBar: {
+      backgroundColor: '#000',
+      fillColor: '#333',
+      height: '3px',
+      showProgressBar: true
+    },
+  };
 
   
   return (
@@ -258,8 +343,51 @@ const YouTube = frontmatter.youtuber
       
       
 
+      <div className="sliderholder" style={{display:'flex', justifyContent:'center', width:'100%', overflow:'hidden', position:'relative', padding:' 0',}}>
+      
+      <TwilightLogo style={{
+      filter:'brightness(0.3)contrast(4.4)hue-rotate(117deg)saturate(9.9)', maxWidth:'50vw', opacity:'.7',
+      position:'absolute', zIndex:'-1', top:'15vh'}} />
+      
+      <div className="RArrow">
+         <span></span>
+        </div>
+    
+    <SimpleReactLightbox>
+          <SRLWrapper options={options} className="">
+          {/* <div className="masonry" style={{}}> */}
+          <div className="horizontal-scroll-wrapper squares" style={{ width:'', padding:'0'}}>
+    
+    
+          <div style={{width:'1000px', height:'1000px'}}></div>
+              
+          {data.allFile.edges.map(edge => {
+          return <GatsbyImage
+          image={edge.node.childImageSharp.gatsbyImageData}
+          srl_gallery_image="true"
+          alt={edge.node.name}
+          key={edge.node.id}
+        />
+        })}
+    
+        </div>
+        </SRLWrapper></SimpleReactLightbox>
+            </div>
+             <GalleryMenu />
 
-      <Portfolio />
+
+
+
+      {/* <Portfolio /> */}
+
+
+
+
+
+
+
+
+
 
         <div name="container21" className="container21" style={{marginTop:'',}}>
 
