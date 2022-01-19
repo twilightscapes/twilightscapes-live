cc8e50a98160f4e7da9bfce07e18656797bfde05
twilightscapes <ihatespam@gmail.com>
2022-01-19 07:09:41 -0600

    slider spacer tweaks

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -349,7 +349,7 @@ const YouTube = frontmatter.youtuber
       // filter:'brightness(0.3)contrast(4.4)hue-rotate(117deg)saturate(9.9)', 
       maxWidth:'50vw',
       opacity:'.5',
-      position:'absolute', zIndex:'-1', top:'15vh',
+      position:'absolute', zIndex:'', top:'15vh',
       hover:''
       }} />
       
